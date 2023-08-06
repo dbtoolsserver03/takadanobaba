6dafff4264270755c5d443990daa00a1f64bd00e
筑紫　与無 <@192.168.2.107>
2023-08-06 16:22:41 +0900

    aa

--- test/src/jp/co/yahoo/study/zhang3/Ta.java
+++ test/src/jp/co/yahoo/study/zhang3/Ta.java
@@ -3,7 +3,7 @@ package jp.co.yahoo.study.zhang3;
 public class Ta {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// /TODO 自動生成されたメソッド・スタブ
 		System.out.println(2);
 	}
 
